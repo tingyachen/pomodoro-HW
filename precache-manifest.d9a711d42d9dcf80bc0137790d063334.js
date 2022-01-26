0edf0f196636725c8683ca9fc3568a5633e60799
tingyachen <90738940+tingyachen@users.noreply.github.com>
2022-01-26 00:44:29 +0000

    Deploying to gh-pages from @ tingyachen/pomodoro-HW@8afa3e2a96028368044b78e8e7a4c5ea88053760 🚀

---
+++ precache-manifest.d9a711d42d9dcf80bc0137790d063334.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b9cad920253439c0f1",
+    "url": "css/app.8c6d35dc.css"
+  },
+  {
+    "revision": "af3c5170a48f2938e7d2",
+    "url": "css/chunk-vendors.989680b1.css"
+  },
+  {
+    "revision": "f0a24bcb83f16be9d327",
+    "url": "css/list.39321df9.css"
+  },
+  {
+    "revision": "dea9842b1b34def05c94e10c636cddbb",
+    "url": "img/tomato.png"
+  },
+  {
+    "revision": "7dd2dd1684304f1dc62218b98ba0d6aa",
+    "url": "index.html"
+  },
+  {
+    "revision": "76b9cad920253439c0f1",
+    "url": "js/app.e51ee311.js"
+  },
+  {
+    "revision": "af3c5170a48f2938e7d2",
+    "url": "js/chunk-vendors.477c529f.js"
+  },
+  {
+    "revision": "f0a24bcb83f16be9d327",
+    "url": "js/list.38594824.js"
+  },
+  {
+    "revision": "9972c1cb2d47328220ce",
+    "url": "js/notfound.597db0e5.js"
+  },
+  {
+    "revision": "cbf815ef04c07edbb8b9441686163a6f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
